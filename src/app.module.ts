9a5c910f694415063e51e1885bc84c59aee90a17
sathyajith <sathyajithsps@gmail.com>
2021-05-20 17:35:06 +0530

    Fixed interface module import error

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { environment, getEnvPath } from '../env';
-import { InterfaceModule } from '@Interface/*';
+import { InterfaceModule } from '@Interface/interface.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
